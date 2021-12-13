fe03dc5caa696c87ea2bbc55746552b85b7d5488
ba323 <59966520+ba323@users.noreply.github.com>
2021-12-13 17:31:19 +0000

    Unity is trash

--- Assets/GunController.cs
+++ Assets/GunController.cs
@@ -5,27 +5,36 @@ using UnityEngine.UI;
 
 public class GunController : MonoBehaviour{
     [Header("Gun Settings")]
-    public float fireRate = 2.0f;
-    public int clipSize = 10;
-    public int reservedAmmoCapacity = 150;
+    public float fireRate;
+    public int clipSize;
+    public int reservedAmmoCapacity;
+    public float range;
+    public bool isRifle;
+
+    public GameObject pistol;
+    public GameObject rifle;
 
     bool _canShoot;
     int _currentAmmo;
     int _currentReserve;
 
     private void Start(){
+
         _currentAmmo = clipSize;
         _currentReserve = reservedAmmoCapacity;
         _canShoot = true;
+        
+        isRifle = false;
     }
 
-    private void update(){
+    private void Update(){
         if(Input.GetMouseButton(0) && _canShoot && _currentAmmo > 0){
             Debug.Log("Shooting gun");
             _canShoot = false;
             _currentAmmo--;
             StartCoroutine(ShootGun());
         }else if(Input.GetKeyDown(KeyCode.R) && _currentAmmo < clipSize && _currentReserve > 0){
+            Debug.Log("reloading");
             int amountNeeded = clipSize - _currentAmmo;
             if(amountNeeded>=_currentReserve){
                 _currentAmmo += amountNeeded;
@@ -33,6 +42,10 @@ public class GunController : MonoBehaviour{
             }else{
                 _currentAmmo += _currentReserve;
                 _currentReserve -= amountNeeded;
+                if(isRifle){
+                    rifle.SetActive(false);
+                    pistol.SetActive(true);
+                }
             }
         }
     }
@@ -43,19 +56,28 @@ public class GunController : MonoBehaviour{
         _canShoot = true;
     }
     void RayCastForEnemy(){
+        Debug.Log("Raycast for enemy");
         RaycastHit hit;
-        if(Physics.Raycast(transform.parent.position, transform.parent.forward, out hit, 1 << LayerMask.NameToLayer("Alien"))){
-            try
-            {
-                Debug.Log("Hit Enemy");
-                 hit.collider.SendMessage("Hit", 1);
-            }
-            catch (System.Exception)
-            {
-            }
-            
+        if(Physics.Raycast(transform.parent.position, transform.parent.forward, out hit)){
+            if(hit.transform.CompareTag("Alien")){
+                float distance =  Vector3.Distance(hit.transform.position, transform.position);
+                if(distance < range){
+                    Debug.Log("Hit Enemy");
+                    try{ 
+                        hit.collider.SendMessage("Hit", 1);
+                    }
+                        catch (System.Exception){
+                    }
+                }else{
+                    Debug.Log("Out of range");
+                }
+            }  
         }
     }
-
+    public void SwapToRifle(){
+        isRifle = true;
+        rifle.SetActive(true);
+        pistol.SetActive(false);
+    }
 }
 
